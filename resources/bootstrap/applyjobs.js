10e18a61f773ef8fa3ce4ca07c87a735e39d94c9
Your Name <you@example.com>
2019-12-05 11:03:43 -0800

    backp

--- resources/bootstrap/applyjobs.js
+++ resources/bootstrap/applyjobs.js
@@ -1,9 +1,6 @@
-
-
-
 $.ajax({
 
-  url: 'http://localhost:3001/viewjobs',
+  url: 'http://localhost:3001/viewjobs' ,
   method: 'GET',
   contentType:'application/json',
   dataType: 'json',
@@ -14,21 +11,24 @@ $.ajax({
    //SSconsole.log(result);
     for (key in result){
      $('#table').append( '<div class="single-post d-flex flex-row">\
-                <div class="thumb">\
-                  <img src="" alt="">\
+                <div>\
+                  <img src="http://localhost:3001/uploads/'+result[key].image+'" height="auto" width="150" alt="">\
                 </div>\
                 <div class="details">\
                   <div class="title d-flex flex-row justify-content-between">\
                     <div class="titles">\
-                      <h4><a href="applyjobs.html/'+result[key].id+'">'+result[key].jobtitle+'</a></h4>\
+                      <h4><a href="applyjobs.html">'+result[key].jobtitle+'</a></h4>\
                       <h6>'+result[key].cname+'</h6></div>\     \
                  \
-                   \
+                    <ul class="btns">\
+                  \
+                      <li><a href="#">Apply</a></li>\
+                    </ul>\
                   </div>\
                   <p>\
-              \
+                <h5><b>Job Description:</b> '+result[key].jobdescription+'</h5>\
                   </p>\
-            \
+                  <h5><b>Experience:</b> '+result[key].experience+'</h5>\
                   <p class="address"><b><span class="lnr lnr-map"></b></span>'+result[key].location+'</p>\
                   <p><b>Education</b></span>: '+result[key].education+'</p>\
                     <p><b>Apply date</b></span>: '+result[key].procedure+'</p>\
